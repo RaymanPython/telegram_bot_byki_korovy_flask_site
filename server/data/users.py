dd2ff5344c4a1642559121cb3e440fde5d317e5a
Ray <ggammi1007@yandex.ru>
2022-05-06 00:07:04 +0300

    Надстройка Сайта

--- server/data/users.py
+++ server/data/users.py
@@ -1,5 +1,6 @@
 import datetime
 import sqlalchemy
+from sqlalchemy import orm
 from .db_session import SqlAlchemyBase
 
 
@@ -10,9 +11,25 @@ class User(SqlAlchemyBase):
                            primary_key=True, autoincrement=True)
     username = sqlalchemy.Column(sqlalchemy.String, nullable=False, unique=True)
     realname = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    created_date = sqlalchemy.Column(sqlalchemy.DateTime,
+                                     default=datetime.datetime.now)
+    records = orm.relation("User_records", back_populates='user')
+
+    def __str__(self):
+        return f'{self.realname, self.records}'
+
+
+class User_records(SqlAlchemyBase):
+    __tablename__ = 'user_records'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer,
+                           primary_key=True, autoincrement=True)
     records = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     created_date = sqlalchemy.Column(sqlalchemy.DateTime,
                                      default=datetime.datetime.now)
+    user_id = sqlalchemy.Column(sqlalchemy.Integer,
+                                sqlalchemy.ForeignKey("users.id"))
+    user = orm.relation('User')
 
     def __str__(self):
-        return f'{self.realname}: {self.records}'
+        return f'{self.records}'
